8c61f6c67e367bd090a4da426ee8fdabc6af4821
Vincarius <davivanci28@gmail.com>
2026-02-21 16:01:40 -0500

    added full StateMachine script

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -1,18 +1,86 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
+    public enum State
+    {
+        idle,
+        walking,
+        swimming,
+        climbing
+    }
+
+    public State currentState = State.idle;
+    Vector3 lastPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        
+    {
+        switch (currentState)
+        {
+            case State.idle: Idle(); break;
+            case State.walking: Walking(); break;
+            case State.swimming: Swimming(); break;
+            case State.climbing: Climbing(); break;
+            default: break;
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "WaterTrigger")
+        {
+           currentState = State.swimming;
+        }
+        else if (other.name == "MountainTrigger")
+        {
+           currentState = State.climbing;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+       currentState = State.walking;
+    }
+
+    void Swimming()
+    {
+        Debug.Log("I am swimming");
+    }
+
+    void Climbing()
+    {
+        Debug.Log("I am climbing");
+    }    
+
+    void Walking()
+    {
+        Debug.Log("I am walking");
+        if (lastPosition == transform.position)
+        {
+            currentState = State.idle;
+        }
+
+        lastPosition = transform.position;
+    }
+
+    void Idle()
+    {
+        Debug.Log("I am idle");
+
+        if (lastPosition != transform.position)
+        {
+            currentState = State.walking;
+        }    
+
+        lastPosition = transform.position;
+    }    
 }
